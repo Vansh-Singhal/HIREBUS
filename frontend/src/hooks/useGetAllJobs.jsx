75e99cd0e1fe32b76991a90fb2870f074fdbf8a9
Vansh Singhal <vansh.singhal@hotmail.com>
2025-02-16 16:21:13 +0530

    URLs Changed

--- frontend/src/hooks/useGetAllJobs.jsx
+++ frontend/src/hooks/useGetAllJobs.jsx
@@ -1,26 +1,29 @@
 import { useEffect } from 'react';
-import {setAllJobs} from '../redux/jobSlice';
+import { setAllJobs } from '../redux/jobSlice';
 import axios from 'axios';
 import { JOB_API_ENDPOINT } from '../utils/constant';
 import { useDispatch, useSelector } from 'react-redux';
 
 const useGetAllJobs = () => {
     const dispatch = useDispatch();
-    const {searchedQuery} = useSelector(state => state.jobs) ? useSelector(state => state.jobs) : "";
+    const { searchedQuery } = useSelector(state => state.jobs) || { searchedQuery: "" }; // Ensure default value
+
     useEffect(() => {
         const fetchAllJobs = async () => {
             try {
-                const res = await axios.get(`${JOB_API_ENDPOINT}/get?keyword=${searchedQuery}`, { withCredentials: true });
+                const queryParam = searchedQuery ? `?keyword=${encodeURIComponent(searchedQuery)}` : "";
+                const res = await axios.get(`${JOB_API_ENDPOINT}/get${queryParam}`, { withCredentials: true });
 
                 if (res.data.success) {
-                    dispatch(setAllJobs(res.data.jobs))
+                    dispatch(setAllJobs(res.data.jobs));
                 }
             } catch (error) {
                 console.log(error);
             }
-        }
+        };
+
         fetchAllJobs();
-    },[dispatch])
+    }, [dispatch, searchedQuery]); // Include searchedQuery in dependencies
 };
 
-export default useGetAllJobs
+export default useGetAllJobs;
